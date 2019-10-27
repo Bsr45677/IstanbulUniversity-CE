acc7062c97587f40fded8b3e9749759f44e5e949
Yunus Emre <49655146+yedhrab@users.noreply.github.com>
2019-10-27 21:10:29 +0300

    🧹 Script temizlendi
    Güncelleme alanı `pre-push` hook'una aktarıldı

--- integrate.sh
+++ integrate.sh
@@ -1,8 +1,3 @@
 python ../YGitBookIntegration/integrate.py . -ll 2
-echo "---
-description: Sitede neler olup bittiğinin 📜 raporudur. Buradaki terimleri 🔍 arama motorunda aratarak sayfaya erişebilirsin
----
-" > CHANGELOG.md && \
-ygitchangelog.exe >> CHANGELOG.md && git add CHANGELOG.md && git commit -m "👀 Neler Değişti alanı yenilendi" && git push -u origin master
-
 bash github .
+# .git/hooks/pre-push
