a3034563fa72c7cdaf3892c1b707db059691f998
Yunus Emre <49655146+yedhrab@users.noreply.github.com>
2019-10-27 23:53:07 +0300

    ✨ Script yenilendi

--- integrate.sh
+++ integrate.sh
@@ -1,3 +1,10 @@
-python ../YGitBookIntegration/integrate.py . -ll 2
+ygitbookintegration . -ll 2
 bash github .
+
 # .git/hooks/pre-push
+#   echo "---
+#   description: Sitede neler olup bittiğinin �📜 raporudur. Buradaki terimleri🔍🔍 arama motorunda aratarak sayfaya erişebili
+#   ---
+#   " > CHANGELOG.md && \
+#   ygitchangelog.exe >> CHANGELOG.md && git add CHANGELOG.md && git commit -m "�👀 Neler Değişti #    alanı yenilendi" && exit 0
+# ypackage gerektirir
